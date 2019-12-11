4bb2c2912a2ae0f009acdddbf571597657d1c960
Vitalik Kim <kimv1086@gmail.com>
2019-12-11 20:45:23 +0600

    added axios & state & componentDidMount

--- src/container/Country.js
+++ src/container/Country.js
@@ -2,10 +2,28 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 class Country extends Component {
+
+    state = {
+        countries: [],
+        countryByName: [],
+        borders: '',
+        countryShown: false,
+    };
+
+    componentDidMount() {
+        axios.get('https://restcountries.eu/rest/v2/all?fields=name').then(response => {
+            this.setState({
+                countries: response.data
+            })
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <div>
-
+<h1>test</h1>
             </div>
         );
     }
