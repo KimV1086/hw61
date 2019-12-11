e412538429ae124f88cea1bcab36d10176da2474
Vitalik Kim <kimv1086@gmail.com>
2019-12-11 21:08:14 +0600

    add countrySelectedHandler

--- src/container/Country.js
+++ src/container/Country.js
@@ -22,10 +22,60 @@ class Country extends Component {
         });
     }
 
+    countrySelectedHandler = name => {
+        axios.get(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
+            const borders = response.data[0].borders;
+            const promises = borders.map(code => {
+                return axios.get('https://restcountries.eu/rest/v2/alpha/' + code);
+            });
+            Promise.all(promises).then(response => {
+                const namesCountry = response.map(country => {
+                    return country.data.name;
+                });
+                if (namesCountry.length === 0) {
+                    this.setState({
+                        borders: "No borders"
+                    })
+                } else if (namesCountry.length > 0) {
+                    this.setState({
+                        borders: namesCountry.join(', ')
+                    })
+                }
+            });
+            this.setState({
+                countryByName: response.data, countryShown: true
+            })
+        })
+    };
+
+
+
     render() {
         return (
             <div>
-<h1>test</h1>
+                <div className="CountryBuild">
+                    <section className="ListCountry">
+                        {this.state.countries.map((country, id) => (
+                            <ListOfCountry
+                                key={id}
+                                title={country.name}
+                                clicked={() => this.countrySelectedHandler(country.name)}
+                            />
+                        ))}
+                    </section>
+                    <section className="CountryWrap">
+                        {this.state.countryByName.map((country, id) => (
+                            <NameOfCountry
+                                key={id}
+                                title={country.name}
+                                flag={country.flag}
+                                capital={country.capital}
+                                population={country.population}
+                                borders={this.state.borders}
+                            />
+                        ))}
+                    </section>
+                </div>
             </div>
         );
     }
